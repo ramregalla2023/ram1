0d15a3a780e29a3edfecb8681fa83bb2d7422c06
ramre <ramre@ram>
2023-01-05 11:36:23 +0530

    First project

---
+++ src/gitdemo1/gitproject.java
@@ -0,0 +1,13 @@
+package gitdemo1;
+
+public class gitproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello world");
+
+	}
+
+}
